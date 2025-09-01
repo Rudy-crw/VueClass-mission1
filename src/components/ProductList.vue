14d861d4c120adafd59af06a64dbbda3694a1df9
Rudy <s60990340@gmail.com>
2025-09-01 13:53:59 +0800

    update

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -4,7 +4,7 @@
     <div class="row">
       <div v-for="product in products" :key="product.id" class="col-md-4 mb-4">
         <div class="card h-100">
-          <img :src="product.image" class="card-img-top" />
+          <img :src="product.image" class="card-img-top" :alt="商品圖片" />
           <div class="card-body">
             <h5 class="card-title">{{ product.name }}</h5>
             <p class="card-text">{{ product.description }}</p>
